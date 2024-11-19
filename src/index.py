9e2a21fbc0ce1fd46aa3feca9b5f80f8d250ae26
Miki Kauste <miki.kauste@gmail.com>
2024-11-19 19:06:16 +0200

    Pieniä parrannuksia testikäyttöliittymään

--- src/index.py
+++ src/index.py
@@ -1,26 +1,28 @@
 from services.studytracker_service import studytracker_service
 
 def main():
-    print("Opintojenseuranta-app")
+    print("-- Opintojenseuranta-app --")
     while True:
         if studytracker_service.user == None:
-            print("Syötä 1 kirjautuaksesi sisään")
-            print("Syötä 2 luodaksesi käyttäjätunnuksen")
-            choice = input("Syötä 1 tai 2: ")
-            if choice == "1":
-                print("Sisäänkirjautuminen")
-                username = input("Käyttäjätunnus: ")
-                password = input("Salasana: ")
-                studytracker_service.login(username, password)
-            elif choice == "2":
-                print("Käyttäjätunnuksen luominen")
-                username = input("Anna väh. 4 merkkiä pitkä käyttäjätunnus: ")
-                password = input("Anna väh. 5 merkkiä pitkä salasana: ")
-                studytracker_service.create_user(username, password)
-            else:
+            choice = input("Syötä 1 kirjautuaksesi sisään tai 2 luodaksesi käyttäjätunnuksen: ")
+            try:
+                if choice == "1":
+                    print("Sisäänkirjautuminen")
+                    username = input("Käyttäjätunnus: ")
+                    password = input("Salasana: ")
+                    studytracker_service.login(username, password)
+                elif choice == "2":
+                    print("-- Käyttäjätunnuksen luominen --")
+                    username = input("Anna väh. 4 merkkiä pitkä käyttäjätunnus: ")
+                    password = input("Anna väh. 5 merkkiä pitkä salasana: ")
+                    studytracker_service.create_user(username, password)
+                else:
+                    print("-- Virheellinen syöte --")
+            except Exception as e:
+                print(e)
                 continue
         if studytracker_service.user:
-            print("Olet kirjautunut sisään")
+            print("-- Olet kirjautunut sisään --")
             choice = input("Syötä 1 uloskirjautuksesi: ")
             if choice == "1":
                 studytracker_service.logout()
